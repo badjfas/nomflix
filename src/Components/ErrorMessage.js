ff9f8b989bc0060704a7cfe79a5dea51ca0ff39b
badjfas <bjwkor@naver.com>
2020-06-27 03:09:31 +0900

    handle error message

---
+++ src/Components/ErrorMessage.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+const Container = styled.div`
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+`;
+
+const Text = styled.span`
+  color: ${props=>props.color};
+  font-weight: 600;
+`;
+
+const ErrorMessage = ({ color, text }) => (
+  <Container>
+    <Text color={color}>{text}</Text>
+  </Container>
+);
+
+export default ErrorMessage;
+
+ErrorMessage.propTypes = {
+  text: PropTypes.string.isRequired,
+};
